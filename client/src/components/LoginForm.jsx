18627cc3b05025071a34266c3ccd030172d48a28
L.J Solis <l.jsolis52@yahoo.com>
2023-11-25 21:34:40 -0600

    Setup the loginform.jsx to accept mutations.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,10 +2,12 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from 'apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
+  const [loginUser] = useMutation(LOGIN_USER);
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,7 +28,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await loginUser({
+        variables: {
+          email: userFormData.email,
+          password: userFormData.password
+        },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
