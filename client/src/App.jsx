38c17866308025e6620c8316183f452c0f4fee54
L.J Solis <l.jsolis52@yahoo.com>
2023-11-25 21:17:48 -0600

    I was committing in the server branch so it was not going through.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,8 +16,9 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
   const token = localStorage.getItem("id_token");
-
+  // retunr the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
