de948568599f158f2f53c37b8cf5e87aa49641dc
L.J Solis <l.jsolis52@yahoo.com>
2023-11-29 21:17:33 -0600

    moved the login and signup form into the pages folder and updated the main.jsx to resemble the connection.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,32 @@
-import ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-
+import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.jsx";
+import SignupForm from "./pages/SignupForm.jsx";
+import LoginForm from "./pages/LoginForm.jsx";
+import SearchBooks from "./pages/SearchBooks.jsx";
+import SavedBooks from "./pages/SavedBooks.jsx";
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
     children: [
       {
+        path: "/signup",
+        element: <SignupForm />,
+      },
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+      {
         index: true,
-        element: <SearchBooks />
-      }, {
-        path: '/saved',
-        element: <SavedBooks />
+        element: <SearchBooks />,
+      },
+      {
+        path: "/saved",
+        element: <SavedBooks />,
       },
     ],
   },
